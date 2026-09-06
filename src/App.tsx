6f99c2212562fc932e89dc770e4785a912a4940f
Steopa2001 <moraru495@gmail.com>
2026-09-06 16:26:43 +0200

    chore: initial porject setup (Vite + React + Typescript + Tailwind)

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-slate-50">
+      <h1 className="text-3xl font-semibold text-slate-900">BaniWise</h1>
+    </main>
+  )
+}
+
+export default App
